efe2104c6fe5ba09b58fe8e7d354997ad085a116
quangtm-xyz <quang.tm.2207@gmail.com>
2025-06-22 22:52:38 +0700

    pull and minos change in fibo method

---
+++ Billard_Booking/src/test_git/Recur_01.java
@@ -0,0 +1,25 @@
+package test_git;
+
+public class Recur_01 {
+
+    public static int fibo(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return fibo(n-1) + fibo(n-2);
+    }
+    public static void main(String[] args) {
+        int [] arr = {
+            fibo(5),
+            fibo(6),
+            fibo(7),
+            fibo(8),
+            fibo(9),
+            fibo(10)
+        };
+        int a = 1;
+        for (Integer i : arr) {
+            System.out.printf("\nFibo %dth: %d", a, i);
+            a++;
+        }
+    }
+}
